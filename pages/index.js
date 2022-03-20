4aeed86df86677c761954134733884198ad93fc5
shayzaran <selahattin.hayzaran@hepsiburada.com>
2022-03-20 15:03:50 +0300

    card component created

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Banner from '../components/banner'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Banner from "../components/banner";
+import Card from "../components/card";
+import styles from "../styles/Home.module.css";
+import coffeeStores from "../data/coffee-stores.json";
 
 export default function Home() {
-
-
   const handleOnBannerBtnClick = () => {
     console.log("Hi");
-  }
+  };
 
   return (
     <div className={styles.container}>
@@ -18,11 +18,25 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick}/>
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+              />
+            );
+          })}
+        </div>
       </main>
-      <div className={styles.heroImage}>
-        <Image src="/static/hero-image.png" width={700} height={400}/>
-      </div>
     </div>
-  )
+  );
 }
